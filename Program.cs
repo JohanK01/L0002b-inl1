6f9ab2e07d537c362254069ac558d8830d9ef02a
johank01 <klausenjohan23@gmail.com>
2022-08-29 14:48:36 +0200

    added some comments to explain the code

--- Program.cs
+++ Program.cs
@@ -1,15 +1,17 @@
 ﻿//Uppgift: Skapa ett kassasytem
 
+//Skapade en  array för valörerna
 int[] money = { 500, 200, 100, 50, 20, 10, 5, 1 };
 
-
+//Indata för pris och betalning
 Console.Write("Ange pris: ");
 int owe = Int32.Parse(Console.ReadLine());
 Console.Write("Betalt: ");
 int payed = Int32.Parse(Console.ReadLine());
-
 int afterTrans = payed - owe;
 
+//Kollar om du har tillräkligt med pengar
+//om du har tillräkligt med pegnar så körs koden 
 if (afterTrans < 0)
 {
     Console.WriteLine($"Det saknas: {-afterTrans} kr");
