f867c4edee23a78cda1e96d2cec8769575c856b1
Johan Klausen <klausenjohan23@gmail.com>
2022-08-29 12:47:37 +0200

    created a foreach that iterates through the change created and tells what to give back

--- Program.cs
+++ Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Initial commit");
+﻿//Uppgift: Skapa ett kassasytem
+
+int[] money = { 500, 200, 100, 50, 20, 10, 5, 1 };
+
+Console.Write("Ange pris: ");
+int owe = Int32.Parse(Console.ReadLine());
+Console.Write("Betalt: ");
+int payed = Int32.Parse(Console.ReadLine());
+
+int afterTrans = payed - owe;
+
+Console.WriteLine("Växel tillbaka: ");
+
+foreach (int i in money)
+{
+	if (afterTrans >= i)
+	{
+		Console.WriteLine($"Antal {i} Lappar: {(afterTrans - afterTrans% i)/i}");
+		afterTrans = afterTrans % i;
+	}
+}
