2ed2c5e38ce394d7b9a60638f8d0b9655883edc1
johank01 <klausenjohan23@gmail.com>
2022-08-29 14:44:56 +0200

    if not enough money a warning comes up

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@
 
 int[] money = { 500, 200, 100, 50, 20, 10, 5, 1 };
 
+
 Console.Write("Ange pris: ");
 int owe = Int32.Parse(Console.ReadLine());
 Console.Write("Betalt: ");
@@ -9,13 +10,22 @@ int payed = Int32.Parse(Console.ReadLine());
 
 int afterTrans = payed - owe;
 
-Console.WriteLine("Växel tillbaka: ");
-
-foreach (int i in money)
+if (afterTrans < 0)
 {
-	if (afterTrans >= i)
-	{
-		Console.WriteLine($"Antal {i} Lappar: {(afterTrans - afterTrans% i)/i}");
-		afterTrans = afterTrans % i;
-	}
+    Console.WriteLine($"Det saknas: {-afterTrans} kr");
 }
+else
+{
+    Console.WriteLine("Växel tillbaka: ");
+
+    foreach (int i in money)
+    {
+        if (afterTrans >= i)
+        {
+            Console.WriteLine($"Antal {i} Kr: {(afterTrans - afterTrans % i) / i}");
+            afterTrans = afterTrans % i;
+        }
+
+    }
+}
+
